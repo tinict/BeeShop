ecd58c8c7f4077384e700a43f69ca43244e7388b
Tín Nguyễn <89054327+tinict@users.noreply.github.com>
2023-08-09 09:46:13 +0700

    8/9/2023 done

--- src/js/Core/CountData.js
+++
@@ -1,8 +0,0 @@
-const CountData = (NameItem) => {
-    let key = JSON.parse(window.localStorage.getItem(NameItem));
-    console.log(key);
-    if (key === null) return 0;
-    else return key.length;
-};
-
-export default CountData;
